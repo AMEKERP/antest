a61966e97315b6e247e9fbbc4ecbc4c585f3334b
Hesham <initume-odoo>
2022-11-24 16:21:53 +0200

    update2

--- charity/controllers/main.py
+++ charity/controllers/main.py
@@ -41,7 +41,7 @@ class CasesCustomerPortal(CustomerPortal):
 
 class Request(http.Controller):
     @http.route('/cases/<model("charity.charity"):case>/', auth='user', website=True, type="http")
-    def case_status(self, case, **kw):
+    def case_status(self,case, **kw):
         # case_rec = request.env['charity.charity'].sudo().search([('create_uid', '=', request.env.user.id)])
         return http.request.render('charity.case_status', {
             'case': case,
@@ -56,10 +56,7 @@ class AddCase(http.Controller):
         print('case form is running ok...', kw)
         currency_rec = request.env['res.currency'].sudo().search([])
         case_record = request.env['charity.cases'].sudo().search([])
-        nation_rec = request.env['res.country'].sudo().search([('name', 'in',
-                                                                ['الكويت', 'المغرب', 'الجزائر', 'مصر', 'تونس',
-                                                                 'السودان', 'الصومال', 'السعودية', 'لبنان', 'سوريا',
-                                                                 'اليمن', 'غير محدد الجنسية'])])
+        nation_rec = request.env['res.country'].sudo().search([('name','in',['الكويت','المغرب','الجزائر','مصر','تونس','السودان','الصومال','السعودية','لبنان','سوريا','اليمن','غير محدد الجنسية'])])
         return http.request.render('charity.case_form1',
                                    {'currency_rec': currency_rec, 'case_record': case_record, 'nation_rec': nation_rec})
 
@@ -69,13 +66,12 @@ class AddCase(http.Controller):
         print('every thing is ok ...', kw)
         # six_months_period_before = (datetime.now() - relativedelta(months=6))
         six_months_period_before = (datetime.now() - timedelta(days=180))
-        six_months_period_after = (datetime.now() + relativedelta(months=6)).date()
+        six_months_period_after = (datetime.now() + relativedelta(months=1)).date()
         print("six_months_Period_before", six_months_period_before)
         print('every thing is ok ...', kw)
-        if request.env['charity.charity'].sudo().search(
-                # [('case_id', '=', case_id), ('create_date', '<', six_months_period_after)]):
-                [('case_id', '=', case_id), ('create_date', '>', six_months_period_before)]):
-                #   [('case_id', '=', case_id), ('x_studio_case_on', '=', True)]):
+        # if request.env['charity.charity'].sudo().search([('case_id', '=', case_id),('create_date', '<', six_months_period_after)]):
+        if request.env['charity.charity'].sudo().search([('case_id', '=', case_id),('create_date', '>', six_months_period_before)]):
+        # if request.env['charity.charity'].sudo().search([('case_id', '=', case_id),('x_studio_case_on', '=', True)]):
             return request.render("charity.case_error", {})
         else:
             request.env['charity.charity'].sudo().create({**kw,
